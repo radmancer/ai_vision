ec89a1c99a3b79831b6375c4b4d629f371f9fdca
James Scott McDowell <mcdowellj87@gmail.com>
2018-01-02 22:47:31 -0500

    This file is no longer necessary.

--- headless_blender.sh
+++
@@ -1,5 +0,0 @@
-#Calling Blender in headless mode to do a geometry “crunch.”
-#NOTE: different unix flavors will have different paths for blender
-#      and the target script will likely be another location.
-#      -b and -P allow the script to run in headless mode.
-/Applications/blender.app/Contents/MacOS/blender -b -P /Users/jamesmcdowell/Desktop/add_cube.py
